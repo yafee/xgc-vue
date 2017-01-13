a92a4f89d4ec3d38c748f8d487acb591e9d74af6
ya_fee <ya_fee@163.com>
2017-01-13 16:55:34 +0800

    test

---
+++ test.js
@@ -0,0 +1,12 @@
+/*
+* @Author: ya_fee
+* @Date:   2017-01-13 16:01:55
+* @Last Modified by:   ya_fee
+* @Last Modified time: 2017-01-13 16:05:26
+*/
+
+'use strict';
+$(function(){
+	var arr = [1,2,3];
+	arr.toString()
+})
